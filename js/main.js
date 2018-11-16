412933e6ff86866a7e490f29587dd721dcb0a747
alexander <alexanderagreda@gmail.com>
2018-11-16 10:02:42 -0500

    end project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$('.carousel').carousel({
-    interval: 2000;
+
+$('a[href="#"]').click(function(){
+    swal({
+        type: 'info',
+        title: '¡Información!',
+        text: 'Este recurso solo representa la apariencia y estructura de contenido propuesta por el Área Sistema Integral de Información en Salud y el diseñador gráfico del equipo de Comunicaciones',
+    });
 });
